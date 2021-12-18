09cef77605ec7f35791d454d3ff6a96a088e4522
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-18 19:26:46 +0330

    fix: remove unnecessary pre-installed apps

--- scripts/01-apk.sh
+++ scripts/01-apk.sh
@@ -4,9 +4,21 @@
 printf 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.15/main\n' >/etc/apk/repositories
 printf 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.15/community\n' >>/etc/apk/repositories
 
-# Update the package list and then upgrade.
 apk update --no-cache
 apk upgrade
 
 apk add docker
 rc-update add docker default
+
+# remove pre-installed apps and daemons
+rc-service virtualbox-guest-additions stop
+rc-update delete virtualbox-guest-additions default
+
+apk del virtualbox-guest-additions
+
+rc-service haveged stop
+rc-update delete haveged default
+
+apk del haveged
+
+apk del man-pages gawk lsof file grep readline mdocml mlocate sysstat findutils sysfsutils dmidecode libmagic sqlite-libs ca-certificates ncurses-libs ncurses-terminfo ncurses-terminfo-base psmisc

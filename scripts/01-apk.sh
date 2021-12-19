f4b335ec1ce817ada2a8475dfe60a6f64db8a7c5
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-19 08:59:28 +0330

    fix: set box vagrant user docker permissions

--- scripts/01-apk.sh
+++ scripts/01-apk.sh
@@ -4,13 +4,21 @@
 printf 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.15/main\n' >/etc/apk/repositories
 printf 'https://mirror.math.princeton.edu/pub/alpinelinux/v3.15/community\n' >>/etc/apk/repositories
 
-# Update the package list and then upgrade.
+# update package cache and then upgrade.
 apk update --no-cache
 apk upgrade
 
+# install docker
 apk add docker
+
+# add docker to OpenRC default run-level
 rc-update add docker default
 
+# set vagrant user permissions to use docker commands without sudo
+addgroup vagrant docker
+
+# remove unnecessary tools already installed by generic base box
 apk del vim man-pages lsof file mdocml mlocate sysstat findutils sysfsutils dmidecode libmagic sqlite-libs ncurses-terminfo psmisc
 
+# clear apk cache to reduce final box size
 rm -rf /var/cache/apk/*

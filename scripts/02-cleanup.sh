8c9b67d8ce7b1064b277845556cca2517a338ed0
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-25 12:03:04 +0330

    fix: improve cleanup script

--- scripts/02-cleanup.sh
+++ scripts/02-cleanup.sh
@@ -5,3 +5,18 @@ apk del virtualbox-guest-additions haveged vim man-pages lsof file mdocml mlocat
 
 # clear apk cache to reduce final box size
 rm -rf /var/cache/apk/*
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -rf /EMPTY
+rm -rf /var/cache/apk/*
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
+sync
+sync
+sync
+sync
+sync
+sync
+
+exit 0

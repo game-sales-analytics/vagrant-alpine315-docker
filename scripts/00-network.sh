05abe2368628bc6dfded1eed09527e1b7b7d531f
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-31 06:08:04 +0330

    fix: disable net-online service auto-start

--- scripts/00-network.sh
+++ scripts/00-network.sh
@@ -4,12 +4,12 @@
 # printf 'keep_network="NO"\n' >>/etc/rc.conf
 
 # Ensure SSHD waits until the network is up and running before launching.
-printf 'rc_need="net-online"\n' >>/etc/conf.d/sshd
+# printf 'rc_need="net-online"\n' >>/etc/conf.d/sshd
 
 # Set up the required interfaces.
 # printf 'interfaces="eth0"\n' >>/etc/conf.d/net-online
 # printf 'timeout=120\n' >>/etc/conf.d/net-online
 
 # Enable the net-online target.
-rc-update add net-online default
-rc-update -u
+# rc-update add net-online default
+# rc-update -u

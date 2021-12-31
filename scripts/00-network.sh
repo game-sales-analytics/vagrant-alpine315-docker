856ae7c6e561809185ff802b16fc33ee864ab8fa
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-12-31 07:52:03 +0330

    fix: set net-online service config

--- scripts/00-network.sh
+++ scripts/00-network.sh
@@ -1,15 +1,21 @@
 #!/bin/sh -eux
 
 # Ensure the network shuts down properly.
-# printf 'keep_network="NO"\n' >>/etc/rc.conf
+printf 'keep_network="NO"\n' >>/etc/rc.conf
+
+# source: https://github.com/OpenRC/openrc/blob/master/conf.d/net-online
+tee /etc/conf.d/net-online <<-EOF
+interfaces="eth0"
+
+include_ping_test=no
+
+timeout=20
+EOF
+chmod 644 /etc/conf.d/net-online
 
 # Ensure SSHD waits until the network is up and running before launching.
-# printf 'rc_need="net-online"\n' >>/etc/conf.d/sshd
-
-# Set up the required interfaces.
-# printf 'interfaces="eth0"\n' >>/etc/conf.d/net-online
-# printf 'timeout=120\n' >>/etc/conf.d/net-online
+printf 'rc_need="net-online"\n' >>/etc/conf.d/sshd
 
 # Enable the net-online target.
-# rc-update add net-online default
-# rc-update -u
+rc-update add net-online default
+rc-update -u
